d9ccc37a31bd6e0463495a16b9d12b2383edd9e0
Alex <worldgoggo05@gmail.com>
2025-04-14 14:01:34 +0900

    feat: develop Apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,25 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { light } from '../scss/MaterialTheme';
 import { useState } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from '@/apollo/client';
 import '../scss/app.scss';
 import '../scss/pc/main.scss';
 import '../scss/mobile/main.scss';
+
 export default function App({ Component, pageProps }: AppProps) {
 	// @ts-ignore
 	const [theme, setTheme] = useState(createTheme(light));
 
-	// Socket.io, Redux, Mui..
+	// Socket.io, Redux, Mui, Apollo Client  .....
 	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<ApolloProvider client={client}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</ApolloProvider>
 	);
 }
+
+// 17-qator client apollo file ichidagi client.ts dan chaqirildi
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,31 @@ import TopProperties from '@/libs/components/homepage/TopProperties';
 import TopAgents from '@/libs/components/homepage/TopAgents';
 import withLayoutMain from '@/libs/components/layout/LayoutHome';
 import useDeviceDetect from '@/libs/hooks/useDeviceDetect';
+import { GET_PROPERTIES } from '@/apollo/user/query';
+import { useQuery } from '@apollo/client';
 
 const Home: NextPage = () => {
 	const device = useDeviceDetect();
+
+	const {
+		loading: getPropertiesLoading,
+		data: getPropertiesData,
+		error: getPropertiesError,
+		refetch: getPropertiesRefetch,
+	} = useQuery(GET_PROPERTIES, {
+		fetchPolicy: 'network-only',
+		variables: {
+			input: {
+				page: 1,
+				limit: 5,
+				sort: 'createdAt',
+				direction: 'DESC',
+				search: {},
+			},
+		},
+	});
+
+	console.log('getPropertiesData =>', getPropertiesData);
 
 	if (device == 'mobile') {
 		return <Stack>HOMEPAGE MOBILE</Stack>;
